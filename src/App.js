14429103273524c39e14fa60944bfa06382e685d
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-06-28 18:55:59 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  } 
+
+  componentDidMount() {
+    var urlApp = 'http://localhost:8085/api/question'
+    fetch(urlApp)
+          .then(res => res.json())
+          .then(json => {
+                this.setState({
+                  isLoaded: true,
+                  items: json,
+                })
+          });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+
+   
+    
+    if(!isLoaded) {
+      return <div>Loading...</div>
+    }
+    
+    return (  
+      <div className="App">
+          <div className="App-header">Personality Test</div>
+          <ol>
+              {items.map(item => (
+                <li key={item.id}>
+                    {item.questionText} <br/>
+                    Category: {item.category.name} <br/>
+                    Options:
+                    <ul>
+                      {item.options.map(opt => (
+                        <li>
+                          {opt.optionText}
+                        </li>
+                      ))} 
+                    </ul>
+                </li>
+              ))}
+          </ol>
+      </div>
+    );
+
+  }
 }
+
+
 
 export default App;
